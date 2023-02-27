da314a224164d362fc60373186870f8460cab0a1
Anuj Gunjad <46643027+anujgunjad@users.noreply.github.com>
2023-02-28 00:11:10 +0530

    async js

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,52 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+//Old school way to call API
+
+//Using XMLHttpRequest
+
+function renderCountry(data, className='') {
+    const html = `
+    <article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${+data.population}</p>
+      <p class="country__row"><span>🗣️</span>${Object.values(data.languages).slice().join(", ")}</p>
+      <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
+    </div>
+    </article>
+    `;
+    
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+}
+
+function getCountryAndNeighbour(country) {
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    request.send();
+    
+    request.addEventListener('load', function () {
+      const data = JSON.parse(this.responseText)[country === 'india' ? 1:0];
+      renderCountry(data);
+
+      let [neighbouringCountry] = data.borders;
+
+      if(!neighbouringCountry) return;
+      
+      const request2 = new XMLHttpRequest();
+      request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbouringCountry}`);
+      request2.send();
+      
+      request2.addEventListener('load', function () {
+        const [data2] = JSON.parse(this.responseText);
+        console.log(data2);
+        renderCountry(data2, 'neighbour');        
+      })
+    })
+}
+
+getCountryAndNeighbour('india');
